cb6716b7bae849cd34871467725f11b021e84e5f
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-08-12 17:24:47 +0800

    Added functionality for Parts Bin
    Added functionality for Parts Bin

--- src/main/java/org/guanzon/auto/controller/parameter/Parts_Bin_Master.java
+++ src/main/java/org/guanzon/auto/controller/parameter/Parts_Bin_Master.java
@@ -6,6 +6,11 @@
  */
 package org.guanzon.auto.controller.parameter;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.guanzon.appdriver.agent.ShowDialogFX;
 import org.guanzon.appdriver.base.GRider;
 import org.guanzon.appdriver.base.MiscUtil;
@@ -22,16 +27,19 @@ import org.json.simple.JSONObject;
 public class Parts_Bin_Master implements GRecord {
 
     GRider poGRider;
-    boolean pbWthParent;
+    boolean pbWtParent;
     int pnEditMode;
+    String psBranchCd;
     String psRecdStat;
 
     Model_Parts_Bin poModel;
     JSONObject poJSON;
+    
 
-    public Parts_Bin_Master(GRider foGRider, boolean fbWthParent) {
+    public Parts_Bin_Master(GRider foGRider, boolean fbWthParent, String fsBranchCd) {
         poGRider = foGRider;
-        pbWthParent = fbWthParent;
+        pbWtParent = fbWthParent;
+        psBranchCd = fsBranchCd.isEmpty() ? foGRider.getBranchCode() : fsBranchCd;
 
         poModel = new Model_Parts_Bin(foGRider);
         pnEditMode = EditMode.UNKNOWN;
@@ -69,45 +77,92 @@ public class Parts_Bin_Master implements GRecord {
 
     @Override
     public JSONObject newRecord() {
-        return poModel.newRecord();
+        poJSON = new JSONObject();
+        try{
+            pnEditMode = EditMode.ADDNEW;
+            org.json.simple.JSONObject obj;
+
+            poModel = new Model_Parts_Bin(poGRider);
+            Connection loConn = null;
+            loConn = setConnection();
+            poModel.newRecord();
+            poModel.setBinID(MiscUtil.getNextCode(poModel.getTable(), "sBinIDxxx", true, loConn, psBranchCd+"BIN"));
+            
+            if (poModel == null){
+                poJSON.put("result", "error");
+                poJSON.put("message", "initialized new record failed.");
+                return poJSON;
+            }else{
+                poJSON.put("result", "success");
+                poJSON.put("message", "initialized new record.");
+                pnEditMode = EditMode.ADDNEW;
+            }
+               
+        }catch(NullPointerException e){
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+        
+        return poJSON;
+    }
+    
+    private Connection setConnection(){
+        Connection foConn;
+        
+        if (pbWtParent){
+            foConn = (Connection) poGRider.getConnection();
+            if (foConn == null) foConn = (Connection) poGRider.doConnect();
+        }else foConn = (Connection) poGRider.doConnect();
+        
+        return foConn;
     }
 
     @Override
     public JSONObject openRecord(String fsValue) {
-        return poModel.openRecord(fsValue);
+        pnEditMode = EditMode.READY;
+        poJSON = new JSONObject();
+        
+        poModel = new Model_Parts_Bin(poGRider);
+        poJSON = poModel.openRecord(fsValue);
+        
+        if("error".equals(poJSON.get("result"))){
+            return poJSON;
+        } 
+        return poJSON;
     }
 
     @Override
     public JSONObject updateRecord() {
-        JSONObject loJSON = new JSONObject();
-
-        if (poModel.getEditMode() == EditMode.UPDATE) {
-            loJSON.put("result", "success");
-            loJSON.put("message", "Edit mode has changed to update.");
-        } else {
-            loJSON.put("result", "error");
-            loJSON.put("message", "No record loaded to update.");
+        poJSON = new JSONObject();
+        if (pnEditMode != EditMode.READY && pnEditMode != EditMode.UPDATE){
+            poJSON.put("result", "error");
+            poJSON.put("message", "Invalid edit mode.");
+            return poJSON;
         }
-        return loJSON;
+        pnEditMode = EditMode.UPDATE;
+        poJSON.put("result", "success");
+        poJSON.put("message", "Update mode success.");
+        return poJSON;
     }
 
     @Override
-    public JSONObject saveRecord() {
-        if (!pbWthParent) {
-            poGRider.beginTrans();
+    public JSONObject saveRecord(){
+        
+        poJSON = validateEntry();
+        if ("error".equals((String) poJSON.get("result"))) {
+            return poJSON;
         }
-
+        
+        if (!pbWtParent) poGRider.beginTrans();
+        
         poJSON = poModel.saveRecord();
 
         if ("success".equals((String) poJSON.get("result"))) {
-            if (!pbWthParent) {
-                poGRider.commitTrans();
-            }
+            if (!pbWtParent) {poGRider.commitTrans();}
         } else {
-            if (!pbWthParent) {
-                poGRider.rollbackTrans();
-            }
+            if (!pbWtParent) {poGRider.rollbackTrans();}
         }
+        
         return poJSON;
     }
 
@@ -126,8 +181,20 @@ public class Parts_Bin_Master implements GRecord {
             if ("error".equals((String) poJSON.get("result"))) {
                 return poJSON;
             }
+            
+            poJSON = validateEntry();
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
 
             poJSON = poModel.saveRecord();
+            if ("success".equals((String) poJSON.get("result"))) {
+                poJSON.put("result", "success");
+                poJSON.put("message", "Deactivation success.");
+            } else {
+                poJSON.put("result", "error");
+                poJSON.put("message", "Deactivation failed.");
+            }
         } else {
             poJSON = new JSONObject();
             poJSON.put("result", "error");
@@ -142,12 +209,23 @@ public class Parts_Bin_Master implements GRecord {
 
         if (poModel.getEditMode() == EditMode.UPDATE) {
             poJSON = poModel.setActive(true);
-
             if ("error".equals((String) poJSON.get("result"))) {
                 return poJSON;
             }
-
+            
+            poJSON = validateEntry();
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+            
             poJSON = poModel.saveRecord();
+            if ("success".equals((String) poJSON.get("result"))) {
+                poJSON.put("result", "success");
+                poJSON.put("message", "Activation success.");
+            } else {
+                poJSON.put("result", "error");
+                poJSON.put("message", "Activation failed.");
+            }
         } else {
             poJSON = new JSONObject();
             poJSON.put("result", "error");
@@ -157,37 +235,38 @@ public class Parts_Bin_Master implements GRecord {
     }
 
     @Override
-    public JSONObject searchRecord(String fsValue, boolean fbByCode) {
-        String lsCondition = "";
-
-        if (psRecdStat.length() > 1) {
-            for (int lnCtr = 0; lnCtr <= psRecdStat.length() - 1; lnCtr++) {
-                lsCondition += ", " + SQLUtil.toSQL(Character.toString(psRecdStat.charAt(lnCtr)));
-            }
-
-            lsCondition = "cRecdStat IN (" + lsCondition.substring(2) + ")";
+    public JSONObject searchRecord(String fsValue, boolean fbByActive) {
+        String lsSQL =    " SELECT "        
+                        + "    sBinIDxxx "  
+                        + "  , sBinNamex "  
+                        + "  , cRecdStat "  
+                        + " FROM bin "       ;
+        
+        if(fbByActive){
+            lsSQL = MiscUtil.addCondition(lsSQL,  " sBinNamex LIKE " + SQLUtil.toSQL(fsValue + "%")
+                                                    + " AND cRecdStat = '1' ");
         } else {
-            lsCondition = "cRecdStat = " + SQLUtil.toSQL(psRecdStat);
+            lsSQL = MiscUtil.addCondition(lsSQL,  " sBinNamex LIKE " + SQLUtil.toSQL(fsValue + "%"));
         }
-
-        String lsSQL = MiscUtil.addCondition(poModel.makeSelectSQL(), " sBinNamex LIKE "
-                + SQLUtil.toSQL(fsValue + "%") + " AND " + lsCondition);
-
+        
+        System.out.println("SEARCH BIN: " + lsSQL);
         poJSON = ShowDialogFX.Search(poGRider,
                 lsSQL,
                 fsValue,
                 "ID»Name",
                 "sBinIDxxx»sBinNamex",
                 "sBinIDxxx»sBinNamex",
-                fbByCode ? 0 : 1);
+                1);
 
         if (poJSON != null) {
-            return poModel.openRecord((String) poJSON.get("sBinIDxxx"));
         } else {
+            poJSON = new JSONObject();
             poJSON.put("result", "error");
-            poJSON.put("message", "No record loaded to update.");
+            poJSON.put("message", "No record loaded.");
             return poJSON;
         }
+        
+        return poJSON;
     }
 
     @Override
@@ -195,5 +274,124 @@ public class Parts_Bin_Master implements GRecord {
         return poModel;
     }
     
-    
+    private JSONObject validateEntry(){
+        JSONObject jObj = new JSONObject();
+        try {
+            
+            if(poModel.getBinID()== null){
+                jObj.put("result", "error");
+                jObj.put("message", "ID cannot be Empty.");
+                return jObj;
+            } else {
+                if(poModel.getBinID().trim().isEmpty()){
+                    jObj.put("result", "error");
+                    jObj.put("message", "ID cannot be Empty.");
+                    return jObj;
+                }
+            }
+            
+            if(poModel.getBinName()== null){
+                jObj.put("result", "error");
+                jObj.put("message", "Description cannot be Empty.");
+                return jObj;
+            } else {
+                if(poModel.getBinName().trim().isEmpty()){
+                    jObj.put("result", "error");
+                    jObj.put("message", "Description cannot be Empty.");
+                    return jObj;
+                }
+            }
+
+            String lsID = "";
+            String lsDesc  = "";
+            String lsSQL = poModel.getSQL();
+            lsSQL = MiscUtil.addCondition(lsSQL, "REPLACE(sBinNamex,' ','') = " + SQLUtil.toSQL(poModel.getBinName().replace(" ",""))) +
+                                                    " AND sBinIDxxx <> " + SQLUtil.toSQL(poModel.getBinID()) ;
+            System.out.println("EXISTING BIN CHECK: " + lsSQL);
+            ResultSet loRS = poGRider.executeQuery(lsSQL);
+
+            if (MiscUtil.RecordCount(loRS) > 0){
+                    while(loRS.next()){
+                        lsID = loRS.getString("sBinIDxxx");
+                        lsDesc = loRS.getString("sBinNamex");
+                    }
+                    
+                    MiscUtil.close(loRS);
+                    
+                    jObj.put("result", "error");
+                    jObj.put("message", "Existing Bin Description Record.\n\nBin ID: " + lsID + "\nDescription: " + lsDesc.toUpperCase() );
+                    return jObj;
+            }
+            
+            lsID = "";
+            lsSQL =   " SELECT "                                      
+                    + "    a.sLocatnID "                              
+                    + "  , a.sLocatnDs "                              
+                    + "  , a.sWHouseID "                              
+                    + "  , a.sSectnIDx "                              
+                    + "  , a.sBinIDxxx "                              
+                    + "  , a.cRecdStat "                              
+                    + "  , b.sBinNamex "                              
+                    + " FROM item_location a "                        
+                    + " LEFT JOIN bin b ON b.sBinIDxxx = a.sBinIDxxx ";
+            //Deactivation Validation
+            if(poModel.getRecdStat().equals("0")){
+                lsSQL = MiscUtil.addCondition(lsSQL, " a.sBinIDxxx = " + SQLUtil.toSQL(poModel.getBinID())) ;
+                System.out.println("EXISTING USAGE OF BIN: " + lsSQL);
+                loRS = poGRider.executeQuery(lsSQL);
+
+                if (MiscUtil.RecordCount(loRS) > 0){
+                        while(loRS.next()){
+                            lsID = loRS.getString("sLocatnID");
+                        }
+
+                        MiscUtil.close(loRS);
+
+                        jObj.put("result", "error");
+                        jObj.put("message", "Existing Bin Usage.\n\nLocation ID: " + lsID + "\nDeactivation aborted.");
+                        return jObj;
+                }
+            }
+            
+            if(pnEditMode == EditMode.UPDATE){
+                lsID = "";
+                lsSQL =   " SELECT "                                      
+                        + "    a.sLocatnID "                              
+                        + "  , a.sLocatnDs "                              
+                        + "  , a.sWHouseID "                              
+                        + "  , a.sSectnIDx "                              
+                        + "  , a.sBinIDxxx "                              
+                        + "  , a.cRecdStat "                              
+                        + "  , b.sBinNamex "                              
+                        + " FROM item_location a "                        
+                        + " LEFT JOIN bin b ON b.sBinIDxxx = a.sBinIDxxx ";
+                
+                lsSQL = MiscUtil.addCondition(lsSQL, " a.sBinIDxxx = " + SQLUtil.toSQL(poModel.getBinID()) 
+                                                        + " AND REPLACE(b.sBinNamex, ' ','') <> " + SQLUtil.toSQL(poModel.getBinName().replace(" ", ""))  
+                                                        //+ " AND a.cRecdStat = '1'"
+                                                        ) ;
+                System.out.println("ITEM LOCATION: EXISTING USAGE OF BIN: " + lsSQL);
+                loRS = poGRider.executeQuery(lsSQL);
+
+                if (MiscUtil.RecordCount(loRS) > 0){
+                        while(loRS.next()){
+                            lsID = loRS.getString("sLocatnID");
+                        }
+
+                        MiscUtil.close(loRS);
+                        
+                        jObj.put("result", "error");
+                        jObj.put("message", "Existing Bin Usage.\n\nLocation ID: " + lsID + "\nChanging of bin description aborted.");
+                        return jObj;
+                }
+            }
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(Vehicle_Make_Master.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        jObj.put("result", "success");
+        jObj.put("message", "Valid Entry");
+        return jObj;
+    }
 }
